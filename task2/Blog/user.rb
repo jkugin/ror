34e71dc5fafe1dfbea337e35a7171235453f17db
Evgeniy Antoniuk <jkugin@rambler.ru>
2016-10-28 16:34:36 +0300

    изменения

--- task2/Blog/user.rb
+++ task2/Blog/user.rb
@@ -1,10 +1,17 @@
 class User
     @@users = []
     
-    def initialize(nikname)
-        @@users << nikname
+    def initialize(nickname)
+        @@users << nickname
+        puts "Пользователь #{nickname} добавлен"
     end
     
-    def delete(nikname)
+    def delete(nickname)
+        @@users.delete(nickname)
+        puts "Пользователь #{nickname} удален"
+    end
+    
+    def view_users
+        puts @@users
     end
 end
